c7bdfd9fddf64529185206be975fc2b117153905
Tucker Hembree <bladeofxp@gmail.com>
2020-03-06 16:24:43 -0500

    added null default in case of immediate cancel @ length input; removed parseInt, as it prevented that null from functioning and it was seemingly redundant; added result variables to all returns, preventing the text box from displaying undefined during a failure state; defined else state for all character set variables, preventing odd no-character set password generation behavior; added additional user alert informing them of their selected password length

--- script.js
+++ script.js
@@ -1,71 +1,75 @@
 // Assignment Code
 
-  // Generate Password; "Something" will be replaced by 
-  // prompt for password length
-  // check to see if user entered length
-  // 4 confirms (special char, numbers, lowercase, uppercase)
-  // generate password following criteria
-  // display password (that's what the 2nd function does)
-  //NOTE: user specifications come from confirms (must save in variables following confirms)
-  // 
-
-
 var generateBtn = document.querySelector("#generate");
 
-// Add event listener to generate button
+// Add event listener to generate button; combined with var generateBtn, as it was redundant (why define a variable for a pathway that you only use once?)
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
+  var passLength = prompt(
+    "How long should you password be? Please choose a value between 8 and 128, inclusive."
+  );
+  if (passLength === null) {
+    return result;
+  }
 
-var passLength =  parseInt (prompt ("How long should you password be? Please choose a value between 8 and 128, inclusive."), 10); 
-if (passLength < 8 | passLength > 128) {
-  alert ("Please choose a value between 8 and 128, inclusive (8 - 128)."); 
-  return; 
-}
+  if (passLength < 8 || passLength > 128) {
+    alert("Please choose a value between 8 and 128, inclusive (8 - 128).");
+    return result;
+  }
 
-if (confirm ("Do you want special characters in your password?")) {
-  var specialChar = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"; 
-  var specCon = "[special characters] "; 
-}
-else {
-  specCon = ""
-}
-if (confirm ("Do you want numbers in your password?")) {
-  var numbers = "0123456789"; 
-  var numCon = "[numbers] "; 
-}
-else {
-  numCon = ""
-}
-if (confirm ("Do you want lowercase letters in your password?")) {
-  var lowerCase = "abcdefghijklmnopqrstuvwxyz"; 
-  var lowCon = "[lowercase letters] "
-}
-else {
-  lowCon = ""
-}
-if (confirm ("Do you want uppercase letters in your password?")) {
-  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";   
-  var upCon = "[uppercase letters] "
-}
-else {
-  upCon = ""
-}
+  if (confirm("Do you want special characters in your password?")) {
+    var specialChar = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    var specCon = "[special characters] ";
+  } else {
+    specialChar = "";
+    specCon = "";
+  }
+  if (confirm("Do you want numbers in your password?")) {
+    var numbers = "0123456789";
+    var numCon = "[numbers] ";
+  } else {
+    numbers = "";
+    numCon = "";
+  }
+  if (confirm("Do you want lowercase letters in your password?")) {
+    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    var lowCon = "[lowercase letters] ";
+  } else {
+    lowerCase = "";
+    lowCon = "";
+  }
+  if (confirm("Do you want uppercase letters in your password?")) {
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var upCon = "[uppercase letters] ";
+  } else {
+    upperCase = "";
+    upCon = "";
+  }
 
-alert ("You have chosen to include the following: " + specCon + numCon + lowCon + upCon)
+  alert("Your password will be " + passLength + " characters long");
+  alert(
+    "You have chosen to include the following: " +
+      specCon +
+      numCon +
+      lowCon +
+      upCon
+  );
 
-// Advice taken from https://stackoverflow.com/a/1349426 and https://stackoverflow.com/a/1497512 
-    var string = "";       
-    var charset = string.concat (specialChar, numbers, lowerCase, upperCase); 
-    if (charset === "") {
-      alert ("Whoops, you didn't select any character sets - there's nothing to display! Please choose at least one character set to include in your password.")
-    }
-    var charLength = charset.length; 
-        result = "";
-    for (var i = 1; i <= passLength; i++) {
-        result += charset.charAt (Math.floor(Math.random() * charLength));
-    }
-    return result; 
+  // Advice taken from https://stackoverflow.com/a/1349426 and https://stackoverflow.com/a/1497512
+  var string = "";
+  var charset = string.concat(specialChar, numbers, lowerCase, upperCase);
+  if (charset === "") {
+    alert(
+      "Whoops, you didn't select any character sets - there's nothing to display! Please choose at least one character set to include in your password."
+    );
+  }
+  var charLength = charset.length;
+  result = "";
+  for (var i = 1; i <= passLength; i++) {
+    result += charset.charAt(Math.floor(Math.random() * charLength));
+  }
+  return result;
 }
 
 // Write password to the #password input
@@ -74,6 +78,4 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
